94269e897f54cf0111b684f02c61ad4743cab21d
Rajandep singh matharu <105154536+Rajn013@users.noreply.github.com>
2023-05-16 16:48:59 +0530

    Add files via upload

---
+++ Untitled68.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q1. What is the purpose of the try statement?
+# 
+
+# The purpose of the try statement is to catch and handle exceptions or errors that may occur during the execution of a block of code, preventing your program from crashing and allowing you to handle the errors gracefully.
+
+# Q2. What are the two most popular try statement variations?
+# 
+
+# try-except: This variation allows you to catch and handle specific exceptions that may occur within the try block. You can specify one or more except clauses, each corresponding to a particular exception type. If any of the specified exceptions are raised, the code within the corresponding except block is executed.
+# 
+# try-finally: This variation ensures that a specific block of code within the finally block is always executed, regardless of whether an exception occurred or not. The finally block is typically used for cleanup operations, such as closing files or releasing resources, that need to be performed regardless of exceptions.
+
+# Q3. What is the purpose of the raise statement?
+# 
+
+# the raise statement enables you to intentionally raise exceptions, triggering error handling mechanisms and facilitating the proper handling of exceptional situations in your code.
+
+# Q4. What does the assert statement do, and what other statement is it like?
+# 
+
+# The assert statement is used for debugging and validating conditions, and it raises an exception if the condition is false, whereas if statements are more general-purpose and provide conditional execution based on a condition without raising an exception specifically for assertion purposes.
+
+# Q5. What is the purpose of the with/as argument, and what other statement is it like?
+# 
+
+# The with/as statement in Python is used for context management, providing a convenient and readable way to handle resource initialization and cleanup. It ensures that resources are properly managed, even in the presence of exceptions, and is often used with objects that implement the context management protocol.
+# 
+# 
+# 
+# 
+# 
+
+# In[ ]:
+
+
+
+
